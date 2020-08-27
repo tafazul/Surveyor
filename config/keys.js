beb583e0ca457ff435f04e4cb0edb0579836839a
Tafazul Nadeem Attar <tafazul93@gmail.com>
2020-08-28 00:03:29 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,16 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
+    module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
   } else {
-    module.exports = require('./dev');
+    module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
   }
   
